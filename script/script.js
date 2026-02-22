82c8e3d88894a69072a5f560d548a103346b79d3
mehedi3322 <mehedihasan88990011@gmail.com>
2026-02-23 00:26:57 +0600

    get all id form the elements

--- script/script.js
+++ script/script.js
@@ -1,1 +1,37 @@
-console.log("hello")
+var cards;
+
+var interviewCount = document.getElementById("interviewCount");
+var rejectedCount = document.getElementById("rejectedCount");
+var totalCount = document.getElementById("totalCount");
+var sectionCount = document.getElementById("sectionCount");
+var noData = document.getElementById("noData");
+
+var tabAll = document.getElementById("tabAll");
+var tabInterview = document.getElementById("tabInterview");
+var tabRejected = document.getElementById("tabRejected");
+// UPDATE COUNT
+function updateCount() {
+
+    cards = document.getElementsByClassName("job-card");
+
+    var interview = 0;
+    var rejected = 0;
+
+    for (var i = 0; i < cards.length; i++) {
+
+        if (cards[i].dataset.status === "interview") {
+            interview++;
+        }
+
+        if (cards[i].dataset.status === "rejected") {
+            rejected++;
+        }
+
+    }
+
+    interviewCount.innerText = interview;
+    rejectedCount.innerText = rejected;
+    totalCount.innerText = cards.length;
+    sectionCount.innerText = cards.length + " Jobs";
+
+}
