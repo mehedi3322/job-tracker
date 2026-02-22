697fbe55b5ab8e30a2f7a11e507c6a9e3ad91b79
mehedi3322 <mehedihasan88990011@gmail.com>
2026-02-23 00:40:46 +0600

    create a updateCount function and set total job count, interview and rejection

--- script/script.js
+++ script/script.js
@@ -33,5 +33,84 @@ function updateCount() {
     rejectedCount.innerText = rejected;
     totalCount.innerText = cards.length;
     sectionCount.innerText = cards.length + " Jobs";
-
 }
+// // FILTER JOBS
+// function filterJobs(type) {
+
+// cards = document.getElementsByClassName("job-card");
+
+// var found = false;
+
+// for (var i = 0; i < cards.length; i++) {
+
+// if (type === "all") {
+// cards[i].style.display = "block";
+// found = true;
+// }
+
+// else if (cards[i].dataset.status === type) {
+// cards[i].style.display = "block";
+// found = true;
+// }
+
+// else {
+// cards[i].style.display = "none";
+// }
+
+// }
+
+// if (!found) {
+// noData.classList.remove("hidden");
+// }
+// else {
+// noData.classList.add("hidden");
+// }
+
+// }
+// BUTTON EVENTS
+// document.addEventListener("click", function (e) {
+
+// if (e.target.closest(".interview")) {
+
+// var card = e.target.closest(".job-card");
+
+// card.dataset.status = "interview";
+
+// updateCount();
+// }
+
+// if (e.target.closest(".rejected")) {
+
+// var card = e.target.closest(".job-card");
+
+// card.dataset.status = "rejected";
+
+// updateCount();
+// }
+
+// if (e.target.closest(".delete")) {
+
+// var card = e.target.closest(".job-card");
+
+// card.remove();
+
+// updateCount();
+
+// filterJobs("all");
+// }
+
+// });
+// TAB EVENTS
+// tabAll.addEventListener("click", function () {
+// filterJobs("all");
+// });
+
+// tabInterview.addEventListener("click", function () {
+// filterJobs("interview");
+// });
+
+// tabRejected.addEventListener("click", function () {
+// filterJobs("rejected");
+// });
+
+// updateCount();
